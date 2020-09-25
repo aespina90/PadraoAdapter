2c81bf6eb145cdbe2319a7f4a28cdc48ed4911ec
DiegoBarney <komyhu@hotmail.com>
2020-09-24 21:56:11 -0300

    Primeiro commit do projeto
    24/09/2020

---
+++ Adapter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //A sacada do Adapter é adaptar uma api ou sistema antigo do sistema,
+            //para uma interface nova ou usual para sua necessidade.
+            RelogioDoSistema data = new RelogioDoSistema();
+
+            Console.WriteLine(data.hoje());
+        }
+    }
+}
---
+++ Adapter/RelogioDoSistema.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adapter
+{
+    class RelogioDoSistema : Relogio
+    {
+
+        public String hoje() {
+
+            return DateTime.Today.ToString();
+        } 
+    }
+}
